130e7b406e50f7cdf30f2a24f997a4b66e5bff2a
Martin Vrublovský <vrublovsky.martin7@gmail.com>
2026-05-25 19:45:06 +0200

    style(client): Loading component

--- client/src/components/state/StateStatusResolver.jsx
+++ client/src/components/state/StateStatusResolver.jsx
@@ -15,17 +15,16 @@ const StateStatusResolver = () => {
   if (status === 'loading...' && !data) {
     return (
       <>
-        <div className="position-relative d-flex justify-content-center align-items-center vh-100">
+        <div
+          className="position-relative d-flex justify-content-center align-items-center"
+          style={{ height: '85vh' }}
+        >
           <div className="position-absolute">
             <Loading size={8} spin={6} />
           </div>
 
           <div className="position-absolute">
-            <Loading size={6} spin={4} />
-          </div>
-
-          <div className="position-absolute">
-            <Loading size={4} spin={2} />
+            <Loading size={6} spin={2} />
           </div>
         </div>
       </>
